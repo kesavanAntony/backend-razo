b5d3a52e4b0ee1d35df05d9f29123a1e1340ecbb
kesavan <136891539+kesavanAntony@users.noreply.github.com>
2024-03-15 12:19:22 +0530

    razo changes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: ["https://magnificent-sunflower-779eb1.netlify.app"],
+    origin: ["https://razo.co.in"],
     methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
     // origin:"*"
